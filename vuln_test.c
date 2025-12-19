ddc232459a727e52c64df3148ff2f75a2b5327fb
zhouxiaobo <alex.screen@staff.eca-jca.edu.au>
2025-12-19 11:16:33 +0800

    Add files via upload

---
+++ vuln_test.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+
+void vulnerable_function(char *user_input) {
+    char buffer[10];
+    // Snyk 会在这里检测到 "Buffer Overflow"
+    // 原因：strcpy 没有检查 user_input 的长度是否超过了 buffer 的大小
+    strcpy(buffer, user_input); 
+}
+
+int main() {
+    char *input = "This string is definitely longer than 10 characters";
+    vulnerable_function(input);
+    return 0;
+}
